ad138fda2c98005b7d50e72acd0eb74ce47a6df8
robert jakub <rj@project2.pl>
2026-03-09 17:42:13 +0100

    Update graylog-sidecar module

--- graylog-sidecar/module.nix
+++ graylog-sidecar/module.nix
@@ -1,51 +1,76 @@
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   cfg = config.services.graylog-sidecar;
   yaml-format = pkgs.formats.yaml { };
   settings-yaml = yaml-format.generate "graylog-sidecar.yml" cfg.settings;
-  filebeat = pkgs.oom-base.filebeat-9_2;
-  auditbeat = pkgs.oom-base.auditbeat-9_2;
+  # collectors
+  filebeat = pkgs.nixdev.filebeat9;
+  auditbeat = pkgs.nixdev.auditbeat9;
+
+  inherit (lib)
+    mkEnableOption
+    mkPackageOption
+    mkIf
+    mkOption
+    types
+    ;
+
 in
 {
   options.services.graylog-sidecar = {
-    enable = lib.mkEnableOption "The Graylog Sidecar is a lightweight configuration management system for log collectors";
-    package = lib.mkPackageOption pkgs.oom-base "graylog-sidecar" { example = "graylog-sidecar"; };
+    enable = mkEnableOption "the Graylog Sidecar";
+    package = mkPackageOption pkgs.nixdev "graylog-sidecar";
 
-    settings = lib.mkOption {
-      type = lib.types.submodule {
+    settings = mkOption {
+      type = types.submodule {
         freeformType = yaml-format.type;
         options = {
-          server_url = lib.mkOption {
-            type = lib.types.str;
-            description = "The Graylog ingest hostname";
+
+          server_url = mkOption {
+            type = types.str;
+            description = "The Graylog ingest URL.";
+            example = "https://<graylogserver>/api";
           };
-          server_api_token = lib.mkOption {
-            type = lib.types.str;
-            description = "The API Token for authenticating";
+
+          server_api_token = mkOption {
+            # should be "secure file";
+            type = types.str;
+            description = "The API Token for authenticating.";
           }; # FIXME
-          node_id = lib.mkOption {
-            type = lib.types.str;
+
+          node_id = mkOption {
+            type = types.str;
             default = "file:/var/lib/graylog-sidecar/node-id";
             description = "Path of the file containing the node-id";
           };
-          log_path = lib.mkOption {
-            type = lib.types.str;
+
+          log_path = mkOption {
+            type = types.str;
             default = "/var/lib/graylog-sidecar/logs";
           };
-          list_log_files = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
+
+          list_log_files = mkOption {
+            type = with types; listOf str;
             default = [ "/var/log" ];
           };
-          tls_skip_verify = lib.mkOption {
-            type = lib.types.bool;
+
+          tls_skip_verify = mkOption {
+            type = types.bool;
             default = true;
           };
-          cache_path = lib.mkOption {
-            type = lib.types.str;
+
+          cache_path = mkOption {
+            type = types.str;
             default = "/var/lib/graylog-sidecar/cache";
           };
-          collector_binaries_accesslist = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
+
+          collector_binaries_accesslist = mkOption {
+            type = with types; listOf str;
             default = [
               "${filebeat}/bin/filebeat"
               "/run/current-system/sw/bin/filebeat"
@@ -53,34 +78,56 @@ in
               "/run/current-system/sw/bin/auditbeat"
             ];
           };
+
         };
       };
     };
-    user = lib.mkOption { type = lib.types.str; default = "graylog"; description = "User account under which graylog-sidecar runs"; };
+
+    user = mkOption {
+      type = types.str;
+      default = "graylog";
+      description = "User account under which graylog-sidecar runs.";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ filebeat auditbeat ];
+  config = mkIf cfg.enable {
+
+    environment.systemPackages = [
+      filebeat
+      auditbeat
+    ];
 
     # reuse graylog user/group
-    users.users = lib.mkIf (cfg.user == "graylog") {
+    users.users = mkIf (cfg.user == "graylog") {
       graylog = {
         isSystemUser = true;
         group = "graylog";
         description = "Graylog server daemon user";
       };
     };
-    users.groups = lib.mkIf (cfg.user == "graylog") { graylog = { }; };
+    users.groups = mkIf (cfg.user == "graylog") { graylog = { }; };
 
     systemd.services.graylog-sidecar = {
       description = "Graylog Sidecar";
       wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      startLimitIntervalSec = 60;
+      startLimitBurst = 3;
       serviceConfig = {
-        User = "${cfg.user}";
         StateDirectory = "graylog-sidecar";
         ExecStart = "${cfg.package}/bin/graylog-sidecar -c ${settings-yaml}";
-        AmbientCapabilities = [ "CAP_AUDIT_CONTROL" "CAP_AUDIT_READ" "CAP_FOWNER" ];
+        AmbientCapabilities = [
+          "CAP_AUDIT_CONTROL"
+          "CAP_AUDIT_READ"
+          "CAP_FOWNER"
+        ];
+        User = "${cfg.user}";
+        LimitCORE = 0;
+        KillSignal = "SIGINT";
+        TimeoutStopSec = "30s";
+        Restart = "on-failure";
       };
+
     };
   };
 }
