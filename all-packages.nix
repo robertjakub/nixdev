2d7c893c0c0c99be27a4240965482942d2376e08
robert jakub <rj@project2.pl>
2026-03-09 17:25:04 +0100

    add new packages.

--- all-packages.nix
+++ all-packages.nix
@@ -3,7 +3,13 @@ let
   pkgs = self.legacyPackages.${system};
 in
 {
-  flame = pkgs.flame;
+  auditbeat9 = pkgs.auditbeat9;
   checkmate = pkgs.checkmate;
   checkmate-capture = pkgs.checkmate-capture;
+  filebeat9 = pkgs.filebeat9;
+  flame = pkgs.flame;
+  graylog-sidecar = pkgs.graylog-sidecar;
+  heartbeat9 = pkgs.heartbeat9;
+  metricbeat9 = pkgs.metricbeat9;
+  packetbeat9 = pkgs.packetbeat9;
 }
