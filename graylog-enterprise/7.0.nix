5082be67b59e4177b33074a61393f78f5017650b
robert jakub <rj@project2.pl>
2026-03-10 17:52:29 +0100

    new packages

---
+++ graylog-enterprise/7.0.nix
@@ -0,0 +1,10 @@
+{ callPackage, lib, ... }:
+let
+  buildGraylog = callPackage ./graylog-enterprise.nix { };
+in
+buildGraylog {
+  version = "7.0.1";
+  hash = "sha256-10b7Bq6HBDDq+dfzUiKeq81EtYhq8tfASSFccQsotS4=";
+  maintainers = with lib.maintainers; [ bbenno ];
+  license = lib.licenses.sspl;
+}
