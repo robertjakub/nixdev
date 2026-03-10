98404ce40dae94c49b06b66980ed4ed60d36b5da
robert jakub <rj@project2.pl>
2026-03-10 23:06:54 +0100

    formatting, maintainer update ((-;

--- graylog/7.0.nix
+++ graylog/7.0.nix
@@ -1,14 +1,10 @@
-{
-  callPackage,
-  lib,
-  ...
-}:
+{ callPackage, lib, ... }:
 let
   buildGraylog = callPackage ./graylog.nix { };
 in
 buildGraylog {
   version = "7.0.5";
   hash = "sha256-ipw8+zQThIiU6KX7ticQJqHFy7bK769DTNa2FIE/kUg=";
-  maintainers = with lib.maintainers; [ bbenno ];
+  maintainers = with lib.maintainers; [ robertjakub ];
   license = lib.licenses.sspl;
 }
