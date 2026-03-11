997ef43a9365101618223a88b5059280fab2297b
robert jakub <rj@project2.pl>
2026-03-11 11:32:40 +0100

    updates

--- graylog-sidecar/module.nix
+++ graylog-sidecar/module.nix
@@ -8,9 +8,6 @@ let
   cfg = config.services.graylog-sidecar;
   yaml-format = pkgs.formats.yaml { };
   settings-yaml = yaml-format.generate "graylog-sidecar.yml" cfg.settings;
-  # collectors
-  filebeat = pkgs.nixdev.filebeat9;
-  auditbeat = pkgs.nixdev.auditbeat9;
 
   inherit (lib)
     mkEnableOption
@@ -24,7 +21,7 @@ in
 {
   options.services.graylog-sidecar = {
     enable = mkEnableOption "the Graylog Sidecar";
-    package = mkPackageOption pkgs.nixdev "graylog-sidecar";
+    package = mkPackageOption pkgs "graylog-sidecar";
 
     settings = mkOption {
       type = types.submodule {
@@ -33,52 +30,50 @@ in
 
           server_url = mkOption {
             type = types.str;
-            description = "The Graylog ingest URL.";
-            example = "https://<graylogserver>/api";
+            description = "Specifies the Graylog API endpoint URL.";
+            example = "https://<graylogserver>/api/";
           };
 
           server_api_token = mkOption {
-            # should be "secure file";
             type = types.str;
-            description = "The API Token for authenticating.";
-          }; # FIXME
+            description = "The API Token for authenticating with the Graylog Server.";
+          };
 
           node_id = mkOption {
             type = types.str;
             default = "file:/var/lib/graylog-sidecar/node-id";
-            description = "Path of the file containing the node-id";
+            description = "Path of the file containing the unique identifier assigned to the Sidecar.";
           };
 
           log_path = mkOption {
             type = types.str;
             default = "/var/lib/graylog-sidecar/logs";
+            description = "The directory where the Sidecar stores its own log files.";
           };
 
           list_log_files = mkOption {
             type = with types; listOf str;
             default = [ "/var/log" ];
+            description = "Log file paths displayed in the Graylog interface.";
           };
 
           tls_skip_verify = mkOption {
             type = types.bool;
             default = true;
+            description = "Whether TLS certificate validation should be skipped.";
           };
 
           cache_path = mkOption {
             type = types.str;
             default = "/var/lib/graylog-sidecar/cache";
+            description = "The directory where the Sidecar stores runtime cache data.";
           };
 
           collector_binaries_accesslist = mkOption {
             type = with types; listOf str;
-            default = [
-              "${filebeat}/bin/filebeat"
-              "/run/current-system/sw/bin/filebeat"
-              "${auditbeat}/bin/auditbeat"
-              "/run/current-system/sw/bin/auditbeat"
-            ];
+            default = [ ];
+            description = "The list of collector binaries that the Sidecar is authorized to execute.";
           };
-
         };
       };
     };
@@ -91,13 +86,7 @@ in
   };
 
   config = mkIf cfg.enable {
-
-    environment.systemPackages = [
-      filebeat
-      auditbeat
-    ];
-
-    # reuse graylog user/group
+    # reuse graylog-server user/group
     users.users = mkIf (cfg.user == "graylog") {
       graylog = {
         isSystemUser = true;
