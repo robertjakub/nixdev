359ed947888c1f5d26fda84739279b5c2ade7336
robert jakub <rj@project2.pl>
2026-03-11 20:53:53 +0100

    Update module.nix

--- graylog/module.nix
+++ graylog/module.nix
@@ -10,28 +10,12 @@ let
   ini-format = pkgs.formats.ini { };
   settings-ini = ini-format.generate "graylog2.conf" cfg.settings;
 
-  confFile = pkgs.writeText "graylog.conf" ''
-    # is_master = ${lib.boolToString cfg.isMaster}
-    # node_id_file = ${cfg.nodeIdFile}
-    password_secret = ${cfg.passwordSecret}
-    root_username = ${cfg.rootUsername}
-    root_password_sha2 = ${cfg.rootPasswordSha2}
-    elasticsearch_hosts = ${lib.concatStringsSep "," cfg.elasticsearchHosts}
-    message_journal_dir = ${cfg.messageJournalDir}
-    mongodb_uri = ${cfg.mongodbUri}
-    plugin_dir = /var/lib/graylog/plugins
-    data_dir = ${cfg.dataDir}
-
-    ${cfg.extraConfig}
-  '';
-
   glPlugins = pkgs.buildEnv {
     name = "graylog-plugins";
     paths = cfg.plugins;
   };
 
 in
-
 {
   ###### interface
 
@@ -57,6 +41,35 @@ in
               description = "Path of the file containing the graylog node-id";
             };
 
+            root_username = lib.mkOption {
+              type = lib.types.str;
+              default = "admin";
+              description = "Name of the default administrator user";
+            };
+
+            message_journal_dir = lib.mkOption {
+              type = lib.types.str;
+              default = "/var/lib/graylog/data/journal";
+              description = "The directory which will be used to store the message journal. The directory must be exclusively used by Graylog and must not contain any other files than the ones created by Graylog itself";
+            };
+
+            plugin_dir = lib.mkOption {
+              type = lib.types.str;
+              default = "/var/lib/graylog/plugins";
+            };
+
+            data_dir = lib.mkOption {
+              type = lib.types.str;
+              default = "/var/lib/graylog/data";
+              description = "Directory used to store Graylog server state.";
+            };
+
+            mongodb_uri = lib.mkOption {
+              type = lib.types.str;
+              default = "mongodb://localhost/graylog";
+              description = "MongoDB connection string. See http://docs.mongodb.org/manual/reference/connection-string/ for details";
+            };
+
           };
         };
       };
@@ -73,12 +86,6 @@ in
           You MUST set a secret to secure/pepper the stored user passwords here. Use at least 64 characters.
           Generate one by using for example: pwgen -N 1 -s 96
         '';
-      };
-
-      rootUsername = lib.mkOption {
-        type = lib.types.str;
-        default = "admin";
-        description = "Name of the default administrator user";
       };
 
       rootPasswordSha2 = lib.mkOption {
@@ -98,30 +105,6 @@ in
         type = lib.types.listOf lib.types.str;
         example = lib.literalExpression ''[ "http://node1:9200" "http://user:password@node2:19200" ]'';
         description = "List of valid URIs of the http ports of your elastic nodes. If one or more of your elasticsearch hosts require authentication, include the credentials in each node URI that requires authentication";
-      };
-
-      dataDir = lib.mkOption {
-        type = lib.types.str;
-        default = "/var/lib/graylog/data";
-        description = "Directory used to store Graylog server state.";
-      };
-
-      messageJournalDir = lib.mkOption {
-        type = lib.types.str;
-        default = "/var/lib/graylog/data/journal";
-        description = "The directory which will be used to store the message journal. The directory must be exclusively used by Graylog and must not contain any other files than the ones created by Graylog itself";
-      };
-
-      mongodbUri = lib.mkOption {
-        type = lib.types.str;
-        default = "mongodb://localhost/graylog";
-        description = "MongoDB connection string. See http://docs.mongodb.org/manual/reference/connection-string/ for details";
-      };
-
-      extraConfig = lib.mkOption {
-        type = lib.types.lines;
-        default = "";
-        description = "Any other configuration options you might want to add";
       };
 
       plugins = lib.mkOption {
@@ -180,8 +163,8 @@ in
         rm -rf /var/lib/graylog/plugins || true
         mkdir -p /var/lib/graylog/plugins -m 755
 
-        mkdir -p "$(dirname ${cfg.nodeIdFile})"
-        chown -R ${cfg.user} "$(dirname ${cfg.nodeIdFile})"
+        mkdir -p "$(dirname ${cfg.settings.node_id_file})"
+        chown -R ${cfg.user} "$(dirname ${cfg.settings.node_id_file})"
 
         for declarativeplugin in `ls ${glPlugins}/bin/`; do
           ln -sf ${glPlugins}/bin/$declarativeplugin /var/lib/graylog/plugins/$declarativeplugin
