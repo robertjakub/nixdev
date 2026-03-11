3a1f40c1360b7055395a4561d1dec2efc77b16f7
robert jakub <rj@project2.pl>
2026-03-11 21:08:30 +0100

    Update all-modules.nix

--- all-modules.nix
+++ all-modules.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ modulesPath, ... }:
 {
+  disabledModules = [ (modulesPath + "/services/logging/graylog.nix") ]; # FIXME
+
   imports = [
     ./checkmate/module.nix
     ./checkmate-capture/module.nix
     ./flame/module.nix
     ./graylog-sidecar/module.nix
+    ./graylog/module.nix
   ];
 }
