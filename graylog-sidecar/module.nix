8ea6452dd22faed6deca89127036d60889036042
robert jakub <rj@project2.pl>
2026-03-13 16:03:47 +0100

    refactor module. almost ready to PR (test.nix req)

--- graylog-sidecar/module.nix
+++ graylog-sidecar/module.nix
@@ -8,101 +8,87 @@ let
   cfg = config.services.graylog-sidecar;
   yaml-format = pkgs.formats.yaml { };
   settings-yaml = yaml-format.generate "graylog-sidecar.yml" cfg.settings;
-  collector_binaries = (map (x: "${lib.getExe x}") cfg.collectors);
-
-  inherit (lib)
-    mkEnableOption
-    mkPackageOption
-    mkIf
-    mkOption
-    types
-    ;
-
 in
 {
   options.services.graylog-sidecar = {
-    enable = mkEnableOption "the Graylog Sidecar";
-    package = mkPackageOption pkgs "graylog-sidecar" { };
+    enable = lib.mkEnableOption "the Graylog Sidecar";
+    package = lib.mkPackageOption pkgs "graylog-sidecar" { };
 
-    collectors = mkOption {
-      type = with types; listOf package;
+    collectors = lib.mkOption {
+      type = with lib.types; listOf package;
       default = [ ];
       description = "The list of collector packages that the Sidecar is authorized to execute.";
     };
 
-    settings = mkOption {
-      type = types.submodule {
+    settings = lib.mkOption {
+      type = lib.types.submodule {
         freeformType = yaml-format.type;
         options = {
 
-          server_url = mkOption {
-            type = types.str;
+          server_url = lib.mkOption {
+            type = lib.types.str;
             description = "Specifies the Graylog API endpoint URL.";
             example = "https://<graylogserver>/api/";
           };
 
-          # server_api_token = mkOption {
-          #   type = types.str;
-          #   description = "The API Token for authenticating with the Graylog Server.";
-          # };
-
-          node_id = mkOption {
-            type = types.str;
+          node_id = lib.mkOption {
+            type = lib.types.str;
             default = "file:/var/lib/graylog-sidecar/node-id";
             description = "Path of the file containing the unique identifier assigned to the Sidecar.";
           };
 
-          log_path = mkOption {
-            type = types.str;
+          log_path = lib.mkOption {
+            type = lib.types.str;
             default = "/var/lib/graylog-sidecar/logs";
             description = "The directory where the Sidecar stores its own log files.";
           };
 
-          list_log_files = mkOption {
-            type = with types; listOf str;
+          list_log_files = lib.mkOption {
+            type = with lib.types; listOf str;
             default = [ "/var/log" ];
             description = "Log file paths displayed in the Graylog interface.";
           };
 
-          tls_skip_verify = mkOption {
-            type = types.bool;
+          tls_skip_verify = lib.mkOption {
+            type = lib.types.bool;
             default = true;
             description = "Whether TLS certificate validation should be skipped.";
           };
 
-          cache_path = mkOption {
-            type = types.str;
+          cache_path = lib.mkOption {
+            type = lib.types.str;
             default = "/var/lib/graylog-sidecar/cache";
             description = "The directory where the Sidecar stores runtime cache data.";
-          };
-
-          collector_binaries_accesslist = mkOption {
-            type = with types; listOf str;
-            default = collector_binaries;
-            description = "The list of collector binaries that the Sidecar is authorized to execute.";
           };
         };
       };
     };
 
-    user = mkOption {
-      type = types.str;
+    user = lib.mkOption {
+      type = lib.types.str;
       default = "graylog";
       description = "User account under which graylog-sidecar runs.";
     };
 
-    APITokenFile = mkOption { type = types.path; };
-
+    APITokenFile = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        Path to a file that contains the API Token for authenticating with the Graylog Server.
+      '';
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
 
     services.graylog-sidecar.settings.server_api_token = "\${API_TOKEN}";
+    services.graylog-sidecar.settings.collector_binaries_accesslist = (
+      map (x: "${lib.getExe x}") cfg.collectors
+    );
 
     environment.systemPackages = cfg.collectors;
 
     # reuse graylog-server user/group
-    users.users = mkIf (cfg.user == "graylog") {
+    users.users = lib.mkIf (cfg.user == "graylog") {
       graylog = {
         isSystemUser = true;
         group = "graylog";
@@ -110,7 +96,7 @@ in
         extraGroups = [ "systemd-journal" ];
       };
     };
-    users.groups = mkIf (cfg.user == "graylog") { graylog = { }; };
+    users.groups = lib.mkIf (cfg.user == "graylog") { graylog = { }; };
 
     systemd.services.graylog-sidecar = {
       description = "Graylog Sidecar";
