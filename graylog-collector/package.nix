b86d17849708eea650d123755c9a0d432a853ab1
robert jakub <rj@project2.pl>
2026-06-22 00:02:56 +0200

    Update package.nix

--- graylog-collector/package.nix
+++ graylog-collector/package.nix
@@ -19,7 +19,7 @@ buildGoModule (finalAttrs: {
   };
 
   # vendorHash = "";
-  vendorHash = "sha256-uRNFzt8sk8z+WJtyzu0R05C+bUwb7PZJfjW8/U4QM/M=";
+  vendorHash = "sha256-KLzeaFbDjCLffgqQ5pDucVuZzKdK9FlVEJ0zppekJLw=";
 
   nativeBuildInputs = [
     go-task
@@ -49,31 +49,32 @@ buildGoModule (finalAttrs: {
     _: {
       preBuild = ''
         (cd builder; go mod download)
-        go get github.com/golang/protobuf@v1.5.4
-        go get github.com/madflojo/testcerts@v1.5.0
-        go get github.com/kylelemons/godebug@v1.1.0
-        go get github.com/BurntSushi/toml@v0.3.1
-        go get github.com/fsnotify/fsnotify@v1.9.0
-        go get github.com/fortytw2/leaktest@v1.3.0
-        go get github.com/google/go-tpm-tools@v0.4.7
-        go get github.com/google/go-cmp@v0.7.0
-        go get go.uber.org/goleak@v1.3.0
-        go get gopkg.in/natefinch/lumberjack.v2@v2.0.0
-        go get gopkg.in/check.v1@v1.0.0-20201130134442-10cb98267c6c
-        go get github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage@v0.153.0
-        go get github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest@v0.153.0
-        go get github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatautil@v0.153.0
-        go get go.opentelemetry.io/otel/metric/x@v0.66.0
-        go get go.opentelemetry.io/otel/sdk/log/logtest@v0.19.0
-        go get go.opentelemetry.io/otel/sdk/log/logtest@v0.20.0
-        go get go.opentelemetry.io/otel/log/logtest@v0.20.0
-        go get go.opentelemetry.io/proto/slim/otlp@v1.10.0
-        go get go.opentelemetry.io/proto/slim/otlp/collector/profiles/v1development@v0.3.0
-        go get go.opentelemetry.io/collector/extension/extensionmiddleware/extensionmiddlewaretest@v0.153.0
-        go get go.opentelemetry.io/collector/extension/zpagesextension@v0.153.0
-        go get go.opentelemetry.io/collector/extension/extensionauth/extensionauthtest@v0.153.0
-        go get go.opentelemetry.io/collector/service/telemetry/telemetrytest@v0.153.0
-        go get go.opentelemetry.io/collector/internal/testutil@v0.153.0
+        go get -u github.com/golang/protobuf@v1.5.4
+        go get -u github.com/madflojo/testcerts@v1.5.0
+        go get -u github.com/kylelemons/godebug@v1.1.0
+        go get -u github.com/BurntSushi/toml@v0.3.1
+        go get -u github.com/fsnotify/fsnotify@v1.9.0
+        go get -u github.com/fortytw2/leaktest@v1.3.0
+        go get -u github.com/google/go-tpm-tools@v0.4.7
+        go get -u github.com/google/go-cmp@v0.7.0
+        go get -u go.uber.org/goleak@v1.3.0
+        go get -u gopkg.in/natefinch/lumberjack.v2@v2.0.0
+        go get -u gopkg.in/check.v1@v1.0.0-20201130134442-10cb98267c6c
+        go get -u github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage@v0.153.0
+        go get -u github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest@v0.153.0
+        go get -u github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatautil@v0.153.0
+        go get -u go.opentelemetry.io/otel/metric/x@v0.66.0
+        go get -u go.opentelemetry.io/otel/sdk/log/logtest@v0.19.0
+        go get -u go.opentelemetry.io/otel/sdk/log/logtest@v0.20.0
+        go get -u go.opentelemetry.io/otel/log/logtest@v0.20.0
+        go get -u go.opentelemetry.io/proto/slim/otlp@v1.10.0
+        go get -u go.opentelemetry.io/proto/slim/otlp/collector/profiles/v1development@v0.3.0
+        go get -u go.opentelemetry.io/collector/extension/extensionmiddleware/extensionmiddlewaretest@v0.153.0
+        go get -u go.opentelemetry.io/collector/extension/zpagesextension@v0.153.0
+        go get -u go.opentelemetry.io/collector/extension/extensionauth/extensionauthtest@v0.153.0
+        go get -u go.opentelemetry.io/collector/service/telemetry/telemetrytest@v0.153.0
+        go get -u go.opentelemetry.io/collector/internal/testutil@v0.153.0
+        go get -u golang.org/x/sync@v0.20.0
       '';
     }
   );
